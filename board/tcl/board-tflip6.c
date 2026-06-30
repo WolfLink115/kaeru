b7d077fcddf7a54c33c42391d79a409b3c83bc1f
WolfLink115 <wolflink747@gmail.com>
2026-06-30 14:31:37 -0700

    Test Port to the TCL Flip 2

---
+++ board/tcl/board-tflip6.c
@@ -0,0 +1,14 @@
+//
+// SPDX-FileCopyrightText: 2026 WolfLink115 <wolflink747@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for TCL Flip 2\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for TCL Flip 2\n");
+}
